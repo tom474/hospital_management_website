9f4f3314238ab36db5196b636195ddde6e6dbc71
locoioioi <118866346+locoioioi@users.noreply.github.com>
2024-08-09 19:06:53 +0700

    feat: finish Sidebar

--- client/src/pages/PatientDetailPage.jsx
+++ client/src/pages/PatientDetailPage.jsx
@@ -1,3 +1,4 @@
+import { useLocation } from "react-router-dom";
 import Sidebar from "../components/patient/detail/Sidebar";
 
 const patient = {
@@ -13,6 +14,11 @@ const patient = {
 };
 
 export default function PatientDetailPage() {
+	const location = useLocation();
+	const queryParams = new URLSearchParams(location.search);
+	// Get the value of the query parameter "option" to determine which content to display
+	const option = queryParams.get("option");
+
 	return (
 		<div className="flex flex-row gap-2">
 			<Sidebar patient={patient} />
