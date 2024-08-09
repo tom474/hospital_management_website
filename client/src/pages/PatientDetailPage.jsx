618f6f40fcfe3454ec9246daaef8014913ef3b4e
locoioioi <118866346+locoioioi@users.noreply.github.com>
2024-08-09 19:08:40 +0700

    feat: finish sidebar (#3)
    * feat: install font awesome icon package
    
    * feat: finish Sidebar

--- client/src/pages/PatientDetailPage.jsx
+++ client/src/pages/PatientDetailPage.jsx
@@ -1,3 +1,28 @@
+import { useLocation } from "react-router-dom";
+import Sidebar from "../components/patient/detail/Sidebar";
+
+const patient = {
+	id: 1,
+	firstName: "John",
+	lastName: "Doe",
+	email: "JohnDoe@gmail.com",
+	phoneNumber: "0901213241",
+	birthDate: "01/01/2000",
+	bloodType: "A+",
+	address: "123 Main St, Springfield, IL",
+	allergies: "Peanuts, Shellfish, Pollen"
+};
+
 export default function PatientDetailPage() {
-	return <div></div>;
+	const location = useLocation();
+	const queryParams = new URLSearchParams(location.search);
+	// Get the value of the query parameter "option" to determine which content to display
+	const option = queryParams.get("option");
+
+	return (
+		<div className="flex flex-row gap-2">
+			<Sidebar patient={patient} />
+			<div></div>
+		</div>
+	);
 }
