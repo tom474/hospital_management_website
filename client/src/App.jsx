29347a73804b0a7d96d7f7bb2ded2e916eb19dbb
tom474 <emsignouc0407@gmail.com>
2024-08-09 12:37:41 +0700

    Initialize project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,3 @@
-function App() {
-	return (
-		<>
-			<div></div>
-		</>
-	);
+export default function App() {
+	return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-
-export default App;
---
+++ client/src/assets/styles/README.txt
@@ -0,0 +1,1 @@
+This folder stores the website's styling files.
