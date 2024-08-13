82fc635e181928a8182ac7e17f38379a858b26c0
tom474 <emsignouc0407@gmail.com>
2024-08-13 09:45:41 +0700

    Complete Patient Page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./pages/Root";
 import Homepage from "./pages/Homepage";
+import PatientPage from "./pages/PatientPage";
+import AddPatientPage from "./pages/AddPatientPage";
 
 export default function App() {
 	const router = createBrowserRouter([
@@ -10,10 +12,23 @@ export default function App() {
 			children: [
 				{
 					index: true,
-					element: <Homepage />
-				}
-			]
-		}
+					element: <Homepage />,
+				},
+				{
+					path: "patient",
+					children: [
+						{
+							index: true,
+							element: <PatientPage />,
+						},
+						{
+							path: "addPatient",
+							element: <AddPatientPage />,
+						},
+					],
+				},
+			],
+		},
 	]);
 
 	return <RouterProvider router={router} />;
