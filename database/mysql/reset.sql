148d70912a2cf535b9f07c5d863c898a79df17cd
Nhat Minh Phan <s3978598@rmit.edu.vn>
2024-08-16 14:41:48 +0700

    Create reset.sql

---
+++ database/mysql/reset.sql
@@ -0,0 +1,8 @@
+-- Drop the database if it exists
+DROP DATABASE IF EXISTS HospitalManagementSystem;
+
+-- Create the database
+CREATE DATABASE HospitalManagementSystem;
+
+-- Use the newly created database
+USE HospitalManagementSystem;
