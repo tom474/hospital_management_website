fc9eb643e542d4aea0f9eb884c6555a91cfda90e
Nhat Minh Phan <s3978598@rmit.edu.vn>
2024-08-23 14:07:15 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const patientRouter = require("./routes/patientRoutes");
 const staffRouter = require("./routes/staffRoutes");
@@ -11,16 +11,15 @@ const scheduleRouter = require("./routes/scheduleRoutes");
 const jobHistoryRouter = require("./routes/jobHistoryRoutes");
 const appointmentRouter = require("./routes/appointmentRoutes");
 
-const mysqlSetup = require("../database/mysql/setup"); // Import the MySQL setup script
-const mongoSetup = require("../database/mysql/setup"); // Import the MongoDB setup script
-
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
+// Middleware setup
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Route setup
 app.use("/patient", patientRouter);
 app.use("/staff", staffRouter);
 app.use("/department", departmentRouter);
@@ -29,24 +28,12 @@ app.use("/schedule", scheduleRouter);
 app.use("/job-history", jobHistoryRouter);
 app.use("/appointment", appointmentRouter);
 
+// Health check route
 app.get("/", (req, res) => {
-	return res.json({ message: "The server is running!" });
+  res.json({ message: "The server is running!" });
 });
 
-async function initializeDatabases() {
-	try {
-		await mysqlSetup(); // Initialize MySQL database
-		await mongoSetup(); // Initialize MongoDB database
-		console.log("Databases initialized successfully!");
-	} catch (error) {
-		console.error("Failed to initialize databases:", error);
-		process.exit(1); // Exit the process with an error
-	}
-}
-
-// Start the server and initialize databases
-initializeDatabases().then(() => {
-	app.listen(port, () => {
-		console.log(`Server running on http://localhost:${port}/`);
-	});
+// Start the server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
 });
