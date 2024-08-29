c28e6c6c2b3b84bceb70717a4871ef5b311022d1
locoioioi <118866346+locoioioi@users.noreply.github.com>
2024-08-29 23:00:44 +0700

    mock data

--- database/mysql/init/mockData.sql
+++ database/mysql/init/mockData.sql
@@ -29,15 +29,7 @@ INSERT INTO Staff (first_name, last_name, email, salary, job_type, qualification
     ('David', 'Anderson', 'david.anderson@hospital.com', 110000.00, 'Doctor', 'MD, Radiology', NULL, 8),
     ('Sophia', 'Moore', 'sophia.moore@hospital.com', 95000.00, 'Nurse', 'RN', NULL, 7),
     ('Liam', 'Lewis', 'liam.lewis@hospital.com', 150000.00, 'Doctor', 'MD, Cardiology', NULL, 1),
-    ('William', 'King', 'william.king@hospital.com', 160000.00, 'Doctor', 'MD, Neurology', NULL, 6),
-    ('Amelia', 'Wright', 'amelia.wright@hospital.com', 102000.00, 'Nurse', 'RN', NULL, 1),
-    ('Lucas', 'Scott', 'lucas.scott@hospital.com', 145000.00, 'Doctor', 'MD, Urology', NULL, 4),
-    ('Charlotte', 'Torres', 'charlotte.torres@hospital.com', 76000.00, 'Administrative', 'BS, Business Admin', NULL, 5),
-    ('Benjamin', 'Carter', 'benjamin.carter@hospital.com', 135000.00, 'Doctor', 'MD, Gastroenterology', NULL, 6),
-    ('Grace', 'Roberts', 'grace.roberts@hospital.com', 115000.00, 'Doctor', 'MD, Endocrinology', NULL, 2),
-    ('Mason', 'Edwards', 'mason.edwards@hospital.com', 128000.00, 'Doctor', 'MD, Pulmonology', NULL, 4),
-    ('Ella', 'Collins', 'ella.collins@hospital.com', 80000.00, 'Nurse', 'RN', NULL, 5),
-    ('Alexander', 'Stewart', 'alexander.stewart@hospital.com', 138000.00, 'Doctor', 'MD, Neurology', NULL, 7);
+    ('William', 'King', 'william.king@hospital.com', 160000.00, 'Doctor', 'MD, Neurology', NULL, 6);
 
 -- Update the Department table with manager IDs
 UPDATE Department SET manager_id = 1 WHERE department_id = 1;
@@ -68,9 +60,6 @@ INSERT INTO Schedule (staff_id, start_time, end_time, date) VALUES
     (8, '08:00:00', '17:00:00', '2024-09-02'),
     (9, '18:00:00', '02:00:00', '2024-09-02'),
     (10, '08:00:00', '17:00:00', '2024-09-03'),
-    (11, '18:00:00', '02:00:00', '2024-09-03'),
-    (12, '08:00:00', '17:00:00', '2024-09-04'),
-    (13, '18:00:00', '02:00:00', '2024-09-04'),
     (2, '08:00:00', '17:00:00', '2024-08-15'),
     (3, '08:00:00', '17:00:00', '2024-08-15'),
     (4, '08:00:00', '17:00:00', '2024-08-14'),
@@ -78,17 +67,7 @@ INSERT INTO Schedule (staff_id, start_time, end_time, date) VALUES
     (7, '18:00:00', '02:00:00', '2024-08-14'),
     (8, '18:00:00', '02:00:00', '2024-08-16'),
     (9, '08:00:00', '17:00:00', '2024-08-18'),
-    (10, '18:00:00', '02:00:00', '2024-08-15'),
-    (11, '08:00:00', '17:00:00', '2024-08-20'),
-    (12, '18:00:00', '02:00:00', '2024-08-20'),
-    (13, '08:00:00', '17:00:00', '2024-08-21'),
-    (14, '18:00:00', '02:00:00', '2024-08-21'),
-    (15, '08:00:00', '17:00:00', '2024-08-22'),
-    (16, '18:00:00', '02:00:00', '2024-08-22'),
-    (17, '08:00:00', '17:00:00', '2024-08-23'),
-    (18, '18:00:00', '02:00:00', '2024-08-23'),
-    (19, '08:00:00', '17:00:00', '2024-08-24'),
-    (20, '18:00:00', '02:00:00', '2024-08-24');
+    (10, '18:00:00', '02:00:00', '2024-08-15');
 
 -- Insert mock data into the Patient table
 INSERT INTO Patient (first_name, last_name, birth_date, address, phone, email, allergies) VALUES
@@ -102,16 +81,7 @@ INSERT INTO Patient (first_name, last_name, birth_date, address, phone, email, a
     ('Dwight', 'Schrute', '1970-01-20', '172 Beet Farm Road', '090145653', 'dwight.schrute@example.com', 'None'),
     ('Pam', 'Beesly', '1979-03-25', '84 Dunder Way', '0901254252', 'pam.beesly@example.com', 'None'),
     ('Jim', 'Halpert', '1978-10-01', '243 Branch Lane', '0906582123', 'jim.halpert@example.com', 'None'),
-    ('Kevin', 'Malone', '1972-06-01', '515 Ice Cream Lane', '0902145554', 'kevin.malone@example.com', 'Lactose'),
-    ('Kelly', 'Kapoor', '1984-02-05', '999 Fashion Ave', '0903154772', 'kelly.kapoor@example.com', 'None'),
-    ('Ryan', 'Howard', '1982-01-10', '101 Temp Street', '0901256347', 'ryan.howard@example.com', 'None'),
-    ('Stanley', 'Hudson', '1958-02-19', '88 Pretzel Way', '0902186493', 'stanley.hudson@example.com', 'None'),
-    ('Phyllis', 'Vance', '1961-07-12', '24 Bob Avenue', '0903257865', 'phyllis.vance@example.com', 'None'),
-    ('Oscar', 'Martinez', '1973-11-20', '202 Accounting St', '0904178291', 'oscar.martinez@example.com', 'None'),
-    ('Toby', 'Flenderson', '1969-09-22', '303 HR Lane', '0905486374', 'toby.flenderson@example.com', 'None'),
-    ('Meredith', 'Palmer', '1965-12-11', '67 Supplier Ave', '0904129875', 'meredith.palmer@example.com', 'Alcohol'),
-    ('Creed', 'Bratton', '1943-11-01', '412 Strange St', '0906582987', 'creed.bratton@example.com', 'None'),
-    ('Jan', 'Levinson', '1967-04-15', '753 Corporate Way', '0902147586', 'jan.levinson@example.com', 'None');
+    ('Kevin', 'Malone', '1972-06-01', '515 Ice Cream Lane', '0902145554', 'kevin.malone@example.com', 'Lactose');
 
 -- Insert mock data into the Appointment table
 INSERT INTO Appointment (patient_id, staff_id, date, start_time, end_time, purpose, status) VALUES
@@ -126,17 +96,7 @@ INSERT INTO Appointment (patient_id, staff_id, date, start_time, end_time, purpo
     (7, 8, '2024-08-18', '10:00:00', '10:45:00', 'Vaccination', 'Scheduled'),
     (8, 1, '2024-08-22', '14:00:00', '14:45:00', 'Consultation', 'Scheduled'),
     (9, 2, '2024-08-23', '11:00:00', '11:30:00', 'Lab Test', 'Scheduled'),
-    (10, 7, '2024-08-24', '09:00:00', '09:30:00', 'Consultation', 'Scheduled'),
-    (3, 2, '2024-09-01', '10:00:00', '10:30:00', 'Consultation', 'Scheduled'),
-    (4, 3, '2024-09-02', '11:00:00', '11:30:00', 'Follow-up', 'Scheduled'),
-    (5, 4, '2024-09-03', '14:00:00', '14:30:00', 'Routine Checkup', 'Scheduled'),
-    (6, 6, '2024-09-04', '09:00:00', '09:30:00', 'Pediatric Checkup', 'Scheduled'),
-    (7, 7, '2024-09-05', '13:00:00', '13:30:00', 'Consultation', 'Scheduled'),
-    (8, 8, '2024-09-06', '15:00:00', '15:30:00', 'Radiology Scan', 'Scheduled'),
-    (9, 9, '2024-09-07', '10:00:00', '10:30:00', 'Consultation', 'Scheduled'),
-    (10, 10, '2024-09-08', '11:00:00', '11:30:00', 'Lab Test', 'Scheduled'),
-    (1, 11, '2024-09-09', '14:00:00', '14:30:00', 'Cardiology Consultation', 'Scheduled'),
-    (2, 12, '2024-09-10', '09:00:00', '09:30:00', 'Dermatology Checkup', 'Scheduled');
+    (10, 7, '2024-08-24', '09:00:00', '09:30:00', 'Consultation', 'Scheduled');
 
 -- Insert mock data into the Treatment table
 INSERT INTO Treatment (patient_id, staff_id, date, description) VALUES
@@ -145,11 +105,7 @@ INSERT INTO Treatment (patient_id, staff_id, date, description) VALUES
     (3, 3, '2024-08-15', 'Follow-up visit; healing well after surgery.'),
     (4, 6, '2024-08-20', 'Pediatric checkup; patient in good health, vaccinations updated.'),
     (5, 1, '2024-08-25', 'Cardiology consultation; ECG showed normal results.'),
-    (6, 7, '2024-08-15', 'Oncology consultation; reviewing chemotherapy progress.'),
-    (7, 8, '2024-08-18', 'Radiology scan; chest X-ray, no abnormalities found.'),
-    (8, 1, '2024-08-22', 'Cardiology follow-up; slight improvement in blood pressure.'),
-    (9, 2, '2024-08-23', 'Neurology follow-up; patient reports reduced frequency of migraines.'),
-    (10, 7, '2024-08-24', 'Oncology follow-up; continuing treatment plan.');
+    (6, 7, '2024-08-15', 'Oncology consultation; reviewing chemotherapy progress.');
 
 -- Re-enable triggers after data insertion
 SET @DISABLE_TRIGGERS = FALSE;
